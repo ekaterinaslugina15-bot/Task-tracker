46cac0dd9135c254286859870679f106489faace
Слугина Екатерина <ekaterinaslugina15@gmail.com>
2026-05-20 16:07:52 +0300

    Delete ~task tracker~ 0.2 directory

--- ~task tracker~ 0.2/JS help/help.js
+++
@@ -1,7 +0,0 @@
-        function toggleFaq(element) {
-            const item = element.parentElement;
-            item.classList.toggle('active');
-            const arrow = element.querySelector('span');
-            arrow.innerHTML = item.classList.contains('active') ? '▲' : '▼';
-        }
-      
